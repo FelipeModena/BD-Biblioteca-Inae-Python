6b3e867bbb36b2d614155ca308ab1eed5cb3cc28
Modena <73952306+FelipeModena@users.noreply.github.com>
2021-07-09 21:02:54 -0300

    uppdate

--- povoamento.py
+++ povoamento.py
@@ -1,16 +1,10 @@
 import sqlite3
 import database
 
-def addMPessoa():
+def addMPessoa(many):
     conn = sqlite3.connect('biblioteca.db')
     c = conn.cursor()
-    many=[("076.728.597-22", "Victor", "da Costa", "24r/04r/1974", "D2", 995, "Bauru", "49070-790", "none"),
-            ("214.755.103-14", "Sônia", "Carolina da Rocha", "25r/03r/1975", "Av. Getúlio Vargas", 996, "Bauru", "98801-027", "ap 12"),
-            ("420.272.234-52", "Lavínia", "da Mata", "03r/09r/1974", "Travessa Pilates", 536, "Bauru", "69918-258"),
-            ("825.029.244-80", "Claudio", "Santos", "10r/09r/1960", "DF", 484, "Bauru", "72578-000"),
-            ("084.313.437-29", "Thiago", "Carvalho", "06r/08r/1977", "Pindaí", 677, "Bauru", "52191-120"),
-            ("311.572.890-53", "Luiza", "Teixeira", "18r/11r/1982", "Pedro Álvares", 883, "Bauru", "57072-312")]
-    c.executemany("""INSERT INTO pessoa VALUES (?,)""", (many))
+    c.executemany("""INSERT INTO pessoa VALUES (?,?,?,?,?,?,?,?,?)""", (many))
     conn.commit()
     conn.close()
 
@@ -24,13 +18,10 @@ def addMSocio():
     conn.commit()
     conn.close()
 
-def addMFuncionario():
+def addMFuncionario(many):
     conn = sqlite3.connect('biblioteca.db')
     c = conn.cursor()
-    many=[("37099909617", 1500.0, "Secretario", "1/1/2001", "825.029.244-80"),
-           ("13859726373", 2000.0, "Bibliotecario", "2/2/2002", "084.313.437-29"),
-            ("24908114106", 1500.0, "Segurança", "3/3/2003", "311.572.890-53")]
-    c.executemany("""INSERT INTO funcionario VALUES (?,)""", (many))
+    c.executemany("""INSERT INTO funcionario VALUES (?,?,?,?,?)""", (many))
     conn.commit()
     conn.close()
 
@@ -72,15 +63,10 @@ def addMLivro():
     conn.commit()
     conn.close()
 
-def addMExemplar():
+def addMExemplar(many):
     conn = sqlite3.connect('biblioteca.db')
     c = conn.cursor()
-    many=[(0, "8585466294"), 
-            (0, "8574061557"), 
-            (0, "8574062197"), 
-            (0, "8565484572"), 
-            (0, "8537809667")]
-    c.executemany("""INSERT INTO exemplar VALUES (?,)""", (many))
+    c.executemany("""INSERT INTO exemplar VALUES (?,?,?)""", (many))
     conn.commit()
     conn.close()
 
@@ -90,7 +76,7 @@ def addMAutor(many):
     c.executemany("""INSERT INTO autor VALUES (?,?,?)""", (many))
     conn.commit()
     conn.close()
-  
+
 def addMCataloga():
     conn = sqlite3.connect('biblioteca.db')
     c = conn.cursor()
@@ -112,30 +98,63 @@ def addMCadastra():
     conn.close()
 
 
-many=[
-      ( "Esopo",      "Esopo"     ,"8585466294" ),
-      ( "Machado",    "Ana Maria" ,"8574061557" ),
-      ( "Moraes, de", "Vinícius"  ,"8574062197" ),
-      ( "Cafaggi",    "Lu"        ,"8565484572" ),
-      ( "Cafaggi",    "Vítor"     ,"8565484572" ),
-      ( "Baum",       "L. Frank"  ,"8537809667" )
-    ]
-addMAutor(many)
+# # autor=[
+# #       ( "Esopo",      "Esopo"     ,"8585466294" ),
+# #       ( "Machado",    "Ana Maria" ,"8574061557" ),
+# #       ( "Moraes, de", "Vinícius"  ,"8574062197" ),
+# #       ( "Cafaggi",    "Lu"        ,"8565484572" ),
+# #       ( "Cafaggi",    "Vítor"     ,"8565484572" ),
+# #       ( "Baum",       "L. Frank"  ,"8537809667" )
+# #     ]
+# # addMAutor(autor)
 
-addMCadastra()
 
-addMCataloga()
 
-addMEmail()
+# addMCadastra()
+# addMCataloga()
+# addMEmail()
 
-addMExemplar()
+exemplar=[
+    ("1",1,"8585466294"),
+    ("2",1,"8585466294"),
+    ("3",1,"8585466294"),
+    ("4",1,"8585466294"),
+    ("5",1,"8585466294"),
+]
+addMExemplar(exemplar)
 
-addMFone()
 
-addMFuncionario()
+pessoa=[
+     ("99999999911","pessoNome1","pessoaSobrenome1","12-12-2012","rua 1",134,"Valinhos","09340000"," "),
+     ("99999999912","pessoNome2","pessoaSobrenome2","12-12-2012","rua 1",134,"Valinhos","09340000"," "),
+     ("99999999913","pessoNome3","pessoaSobrenome3","12-12-2012","rua 1",134,"Valinhos","09340000"," "),
+     ("99999999914","pessoNome4","pessoaSobrenome4","12-12-2012","rua 1",134,"Valinhos","09340000"," "),
+     ("99999999915","pessoNome5","pessoaSobrenome5","12-12-2012","rua 1",134,"Valinhos","09340000"," "),
+     ("99999999916","pessoNome6","pessoaSobrenome6","12-12-2012","rua 1",134,"Valinhos","09340000"," "),
+     ("99999999917","pessoNome7","pessoaSobrenome7","12-12-2012","rua 1",134,"Valinhos","09340000"," "),
+     ("99999999918","pessoNome8","pessoaSobrenome8","12-12-2012","rua 1",134,"Valinhos","09340000"," "),
+ ]
+addMPessoa(pessoa)
 
-addMLivro()
+funcionario=[
+    ("1231232",1.32,"bibliotecario","12-12-2012","99999999917"),
+    ("1231231",1.211,"almoxarife","12-12-2012","99999999918"),
+    ("1234566",1.3333,"faxina","12-12-2012","99999999916"),
+]
+addMFuncionario(funcionario)
 
-addMPessoa()
+# addMFone()
 
-addMSocio()
+
+
+# livro=[
+#     (""),
+# ]
+# addMLivro(livro)
+
+
+
+# cadastra=[
+#     (""),
+# ]
+# addMSocio()
